62d89768c1747e4f2e38adaad157cc80177f4bc3
denijosua <denijoshuasamosir@gmail.com>
2020-06-22 23:30:34 +0700

    security file tambahan

--- security/security.java
+++ security/security.java
@@ -10,3 +10,33 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true
+)
+class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/",
+                        "/favicon.ico",
+                        "/*/.png",
+                        "/*/.gif",
+                        "/*/.svg",
+                        "/*/.jpg",
+                        "/*/.html",
+                        "/*/.css",
+                        "/*/.js")
