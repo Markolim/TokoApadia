6fb452871918043232b2995f7433433ac8d723c6
denijosua <denijoshuasamosir@gmail.com>
2020-06-22 23:38:17 +0700

    penambahan class baru

---
+++ controler/main/test/administrator.java
@@ -0,0 +1,18 @@
+@RestController
+@RequestMapping("/shoppingcart")
+//ini untuk menambah case ketidka ada kondisi pembelian lebih dari satu
+public class ContohKondisi {
+
+    public static void main(String[] args) {
+        boolean kondisi;
+
+        kondisi= (1+1) == 2 && 2>1;
+
+        if (kondisi){
+            System.out.println("Jawaban anda benar...");
+        }
+        else {
+            System.out.println("Jawaban anda benar salah...");
+        }
+    }
+}
