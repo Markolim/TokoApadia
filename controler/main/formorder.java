613c6fde9086245325ca9a6236ecbda3bc51e033
Markolim <54051867+Markolim@users.noreply.github.com>
2020-06-22 23:41:02 +0700

    menambahkan class cetak angka

--- controler/main/formorder.java
+++ controler/main/formorder.java
@@ -24,3 +24,25 @@ public class JavaExample {
     	obj.calculate(2000, 5, .08, 12);
     }
 }
+class CetakAngka{
+/*contoh membuat variabel dan scanner
+ contoh komentar*/
+    public static void main(String[] argumen){
+
+        for(int i=0; i <= 10; i++){
+            System.out.print( i + " " );
+        }
+
+    }
+}
+
+public class AddTwoNumbers {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
