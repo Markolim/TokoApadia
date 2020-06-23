dc667bdb925b10ef4dd1bf79f5ff54cb0325d706
Fitri <38976894+FitrianiGitana3@users.noreply.github.com>
2020-06-23 10:56:26 +0700

    Update customer.java

--- controler/main/exmple/customer.java
+++ controler/main/exmple/customer.java
@@ -1,5 +1,18 @@
+//impoert
+//librarry
+
+
+
 public class customer {
     public static void main(String[] args) {
+        Boolean bukan_dosen = true;
+
+        if (bukan_dosen){
+            System.out.println("Tidak boleh mengakses halaman dosen...");
+        }
+        else {
+            System.out.println("Boleh mengakses halaman dosen...");
+        }
         
     }
     
