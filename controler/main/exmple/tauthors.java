31e582d3fc740ec73a4003d4a5e2e9139a1574bb
Rolay <vinny.sihombing@yahoo.com>
2020-06-23 10:27:15 +0700

    Update tauthors.java

--- controler/main/exmple/tauthors.java
+++ controler/main/exmple/tauthors.java
@@ -4,4 +4,8 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 public class tauthors extends HttpServlet
 {
+  public void doPost(HttpServletRequest req,HttpServletResponse res) throws ServletException,IOException
+	{
+  
+  }
 }
