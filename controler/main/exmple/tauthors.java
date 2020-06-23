a6b9020a93c15ee2c4d96c39c532d3ee1cffc3d6
Masnikarelain <47467309+Masnika@users.noreply.github.com>
2020-06-23 23:42:11 +0700

    add class line item

--- controler/main/exmple/tauthors.java
+++ controler/main/exmple/tauthors.java
@@ -63,3 +63,23 @@ public class tauthors extends HttpServlet
 //fungsi 
 //untuk 
 //tauthors
+import java.util.*;
+class LineItem{
+	private int[] itemCost = {50, 25, 75, 100, 10};
+	private String[] productNames = {"Bananas","Apples","Oranges","Grapefruits","Cherries"};
+	private int[] shoppingCartQuantity = new int[5];
+	public lineItem(string[] productNames, int[] itemCost, int[] shoppingCartQuantity){
+		this.itemCost = itemCost;
+		this.productNames = productNames;
+		this.shoppingCartQuantity = shoppingCartQuantity;
+	} 
+	public int[] getShoppingCartQuantity(){
+		return shoppingCartQuantity;
+	}
+	public int[] getItemCost(){
+		return itemCost;
+	}
+	public String[] getProductNames(){
+		return productNames;
+	}
+}
