0e8e3734b5d93fe34cd07ba3e4bdec8c047da7bf
Ventina Limbong <ventinabong@gmail.com>
2020-06-23 12:23:56 +0700

    delete  something

--- controler/main/test/UserController.java
+++ controler/main/test/UserController.java
@@ -1,3 +1,4 @@
+//make class for create user
 public class CreateUser {
 
     public static void main(String[] args) {
@@ -5,10 +6,9 @@ public class CreateUser {
         double gravitasi = 9.8;
         String nama = "Peter Parker";
         Boolean bukan_dosen = false;
-        String[] User = new String[]{"Ant Man", "Captain America", "Spiderman", "Hulk", "Thor", "Iron Man"};
+        String[] User = new String[]{"Ant Man", "Captain America", "Spiderman"};
 
         System.out.println("umur: " + umur);
-
         System.out.println("gravitasi: " + gravitasi);
 
         System.out.println(nama instanceof String);
@@ -18,9 +18,7 @@ public class CreateUser {
         System.out.println("User 1: " + User[0]);
         System.out.println("User 2: " + User[1]);
         System.out.println("User 3: " + User[2]);
-        System.out.println("User 4: " + User[3]);
-        System.out.println("User 5: " + User[4]);
-        System.out.println("User 6: " + User[5]);
+        
     }
 
 }
