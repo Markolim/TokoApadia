89dc7740e65656bb801fa753350d4c8fe3870c24
Ventina Limbong <ventinabong@gmail.com>
2020-06-23 12:17:14 +0700

    modify something

--- controler/main/exmple/ttsg.java
+++ controler/main/exmple/ttsg.java
@@ -4,15 +4,17 @@ import java.io.*;
 import java.sql.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+
 public class ttsng extends HttpServlet
 {
-  public void doGet(HttpServletRequest req,HttpServletResponse res) throws ServletException,IOException
-  {
+	
+	public void doGet(HttpServletRequest req,HttpServletResponse res) throws ServletException,IOException
+  	{
   	Connection con=null;
 	Statement stmt=null;
 	ResultSet rs=null;
-	res.setContentType("text/html");
-	PrintWriter pw=res.getWriter();
+	// res.setContentType("text/html");
+	// PrintWriter pw=res.getWriter();
 	try
 	{
 		Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
@@ -20,12 +22,14 @@ public class ttsng extends HttpServlet
 		stmt=con.createStatement();
 		String qry="select s1 from music group by s1 order by sum(nocs) desc ";
 		rs=stmt.executeQuery(qry);
+		
 		pw.println("<br>");
 		pw.println("<pre><b><font size=6>"+"\t\t\t"+"<u>"+"TOP TEN SONGS"+"</pre></u></b></font>");
 		pw.println("<form  action=http://localhost:8080/servlet/ttsng1 method=get>");
 		pw.println("<br><font size=4><b>");
 		//pw.println("<pre>"+"TITLE\t\t\t\tAUTHOR\t\t\t\tPUBLISHER NAME\t\t\t SUBJECT\t\t\tRATE"+"</pre></font></b>");
 		pw.println("<pre><select name=selectbk size=5>");
+		
 		while(rs.next())
 		{
 		String t=rs.getString(1);
@@ -36,13 +40,5 @@ public class ttsng extends HttpServlet
 		pw.println("<font size=3><br><br><input type=submit value=\"Show the list\"></font></form>");}
 	catch(ClassNotFoundException e){}
 	catch(SQLException e){}
-	finally
-	{
-	try
-	{ if (con!=null)
-	 con.close();
-	}catch(SQLException e){}}
-}
-  
   }
 }
