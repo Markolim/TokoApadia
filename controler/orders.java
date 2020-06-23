03252d88b79e72c174322dc5144990346e16fbbc
Masnikarelain <47467309+Masnika@users.noreply.github.com>
2020-06-23 23:33:31 +0700

    add class

--- controler/orders.java
+++ controler/orders.java
@@ -21,3 +21,24 @@ public class CreateOrder {
         }
     }
 }
+//tambahan class cart
+class Cart {
+
+    List<Product> cartItems = new ArrayList<Product>();
+    
+    public void addProductToCartByPID(int pid) {
+        Product product = getProductByProductID(pid);
+        addToCart(product);
+    }
+
+    private Product getProductByProductID(int pid) {
+        Product product = null;
+        List<Product> products = new Products().getProducts();
+        for (Product prod: products) {
+            if (prod.getPid() == pid) {
+                product = prod;
+                break;
+            }
+        }
+        return product;
+    }
