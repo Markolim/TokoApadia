b6cc167ea792c4ee397fbe4c6f556014c3ee36ea
Rolay <vinny.sihombing@yahoo.com>
2020-06-23 10:27:32 +0700

    Update tauthors.java

--- controler/main/exmple/tauthors.java
+++ controler/main/exmple/tauthors.java
@@ -6,6 +6,10 @@ public class tauthors extends HttpServlet
 {
   public void doPost(HttpServletRequest req,HttpServletResponse res) throws ServletException,IOException
 	{
-  
+  Connection con=null;
+	Statement stmt=null;
+	ResultSet rs=null;
+	res.setContentType("text/html");
+	PrintWriter pw=res.getWriter();
   }
 }
