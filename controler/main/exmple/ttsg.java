f00893840e67f5706119bf2ed5ad27c02bb515f2
Rolay <vinny.sihombing@yahoo.com>
2020-06-23 10:24:19 +0700

    Update ttsg.java

--- controler/main/exmple/ttsg.java
+++ controler/main/exmple/ttsg.java
@@ -6,5 +6,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 public class ttsng extends HttpServlet
 {
-
+  public void doGet(HttpServletRequest req,HttpServletResponse res) throws ServletException,IOException
+	{
+  }
 }
