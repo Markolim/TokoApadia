6ae813af4558427a193bde4235570b6fb56b050a
Rolay <vinny.sihombing@yahoo.com>
2020-06-23 09:58:19 +0700

    import library

--- controler/main/test/administrator.java
+++ controler/main/test/administrator.java
@@ -1,3 +1,17 @@
+package com.agung.controller;
+
+//import library
+import java.security.Principal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+ 
+/**
+ *
+ * @author Agung Setiawan
+ */
+
 @RestController
 @RequestMapping("/shoppingcart")
 //ini untuk menambah case ketidka ada kondisi pembelian lebih dari satu
